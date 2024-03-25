e1fe74f63a33b49696503cce606e44a429f0571a
vincentfaure <faurev@3il.fr>
2024-03-25 21:52:50 +0100

    Réalisation : Algorithme de dijkstra

--- src/fr/ecole3il/rodez2023/carte/chemin/algorithmes/AlgorithmeDijkstra.java
+++ src/fr/ecole3il/rodez2023/carte/chemin/algorithmes/AlgorithmeDijkstra.java
@@ -1,7 +1,11 @@
 package fr.ecole3il.rodez2023.carte.chemin.algorithmes;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
+import java.util.PriorityQueue;
 import java.util.TreeMap;
 
 import fr.ecole3il.rodez2023.carte.elements.Graphe;
@@ -12,16 +16,54 @@ public class AlgorithmeDijkstra<E> implements AlgorithmeChemin<E> {
 	@Override
 	public List<Noeud<E>> trouverChemin(Graphe<E> graphe, Noeud<E> depart, Noeud<E> arrivee) {
 		
-		Map<Noeud<E>, Double> coutByNoeud = new TreeMap<>();
+		Map<Noeud<E>, Double> couts = new TreeMap<>();
+		Map<Noeud<E>, Noeud<E>> predecesseurs = new TreeMap<>();
+		
+		// File de prioritée
+		PriorityQueue<Noeud<E>> filePriorite = new PriorityQueue<>(Comparator.comparingDouble(couts::get));
 				
 		for (Noeud n : graphe.getNoeuds()) {
-			coutByNoeud.put(n, Double.MAX_VALUE);
+			couts.put(n, Double.MAX_VALUE);
+			predecesseurs.put(n, null);
 		}
 		
-		coutByNoeud.put(depart, 0.0);
-		
-		// TODO Auto-generated method stub
-		return null;
+		couts.put(depart, 0.0); // Noeud de départ cout = 0
+        filePriorite.add(depart);
+        
+        Noeud<E> noeudActuel = filePriorite.poll();
+        
+        while(!filePriorite.isEmpty() && !arrivee.equals(noeudActuel)) {
+        	
+        	for(Noeud voisin : noeudActuel.getVoisins()) {
+        		double nouveauCout = couts.get(noeudActuel) + graphe.getCoutArete(noeudActuel, voisin);
+                if (nouveauCout < couts.get(voisin)) {
+                    couts.put(voisin, nouveauCout);
+                    predecesseurs.put(voisin, noeudActuel);
+                    filePriorite.add(voisin);
+                }
+        	}
+        	
+        	noeudActuel = filePriorite.poll();
+        }
+        
+        return reconstructionChemin(arrivee, predecesseurs);
+	}
+	
+	/**
+	 * 
+	 * @param arrivee
+	 * @param predecesseurs
+	 * @return
+	 */
+	private List<Noeud<E>> reconstructionChemin(Noeud<E> arrivee, Map<Noeud<E>, Noeud<E>> predecesseurs){
+		List<Noeud<E>> chemin = new ArrayList<>();
+        Noeud<E> noeud = arrivee;
+        while (noeud != null) {
+            chemin.add(noeud);
+            noeud = predecesseurs.get(noeud);
+        }
+        Collections.reverse(chemin);
+        return chemin;
 	}
 
 }
--- src/fr/ecole3il/rodez2023/carte/elements/Noeud.java
+++ src/fr/ecole3il/rodez2023/carte/elements/Noeud.java
@@ -3,25 +3,49 @@ package fr.ecole3il.rodez2023.carte.elements;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Représente un noeud
+ * @param <E>
+ */
 public class Noeud<E> {
 	
+	/**
+	 * Valeur du noeud
+	 */
 	private E valeur;
 	
+	/**
+	 * Liste des voisins du noeud
+	 */
 	private List<Noeud<E>> voisins;
 
+	/**
+	 * Construit un noeud
+	 * @param valeur valeur du noeud à créer
+	 */
 	public Noeud(E valeur){
 		this.valeur = valeur;
 		voisins = new ArrayList<>();
 	}
 
+	/**
+	 * @return la valeur du noeud
+	 */
 	public E getValeur() {
 		return valeur;
 	}
 
+	/**
+	 * @return les voisins du noeud
+	 */
 	public List<Noeud<E>> getVoisins() {
 		return voisins;
 	}
 	
+	/**
+	 * Ajoute un Noeud voisins au noeud courant
+	 * @param voisin
+	 */
 	public void ajouterVoisin(Noeud<E> voisin) {
 		voisins.add(voisin);
 	}
